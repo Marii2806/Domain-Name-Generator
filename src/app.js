89bd0b8396b1706618a8ba2ffd372879bb6d16d1
Marii2806 <7542600354g@gmail.com>
2022-07-29 20:25:55 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  domainName();
 };
+let pronoun = ["the", "our"];
+let adj = ["excellent", "good", "bad", "big"];
+let noun = ["jogger", "racoon", "fox", "bear", "elk", "buddy"];
+let ext = [".com", ".net", ".us", ".io", ".ru", ".it"];
+
+function domainName() {
+  let names = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let d = 0; d < noun.length; d++) {
+        for (let b = 0; b < ext.length; b++) {
+          names.push(pronoun[i] + adj[j] + noun[d] + ext[b]);
+        }
+      }
+    }
+  }
+  document.querySelector("#domain").innerHTML = names;
+}
+console.log("");
